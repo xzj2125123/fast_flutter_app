3279616dafe00cac64c484099fab50ad0a805055
xiezj <649721004@qq.com>
2020-06-06 18:22:23 +0800

    星星

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,22 +1,35 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'dart:convert';
+import 'package:encrypt/encrypt.dart';
+import 'package:encrypt/encrypt.dart' as prefix0;
 
 import 'package:flutter/material.dart';
+import 'package:flutter_cipher/flutter_cipher.dart' as prefix1;
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:xzj_fast_flutter_app/main.dart';
 import 'package:xzj_fast_flutter_app/net/ParameterUtils.dart';
-
+import 'package:flutter_cipher/flutter_cipher.dart';
 void main() {
+   const String keys = "123456789abcdef0";
   //hC8HflqwiZ5ppWXIC4mzcsQBonNA7Q72dtgotCr1JbhUtaGqKgsId6Wurj2XHY07A9NxhWs6ZwDtLi3SG2402w==
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    var string=  ParameterUtils.ebncrypDataCopy({
-      "channel_id": "1"}, "291557", 1591421097116);
-    print("------------------$string------------------------");
+   testWidgets('Counter increments smoke test', (WidgetTester tester)  {
+    String content = json.encode( {"channel_id": "1","uid":"291557","time":"1591421097116"});
+    try {
+      print("------------------$content------------------------");
+      final key = prefix0.Key.fromUtf8(keys);
+      final encrypter = Encrypter(prefix0.AES(key, mode: prefix0.AESMode.cbc));
+      final encrypted = encrypter.encrypt(content, iv: prefix0.IV.fromUtf8(keys));
+      print("------------------$encrypted------------------------");
+      print("------------------${encrypted.base64}------------------------");
+    } catch (err) {
+      print("aes encode error:$err");
+      print("------------------$content------------------------");
+    }
+
+    prefix1.Key key = prefix1.Key.fromUtf8(keys);
+    Symmetry aes = Cipher.getSymmetryInstance(prefix1.AES(key));
+    var encrypted = aes.encrypt(utf8.encode(content),iv: prefix1.IV.fromUtf8(keys));
+
+    print("------------------${encrypted.base64}------------------------");
   });
+
 }
